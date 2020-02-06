ef72bb4aa7f873f5b71cc1aae8db03a8f1cb6a77
didier-wg2 <54026559+didier-wg2@users.noreply.github.com>
2020-02-06 16:27:20 +0100

    Update wgtwo dependency version (#3)

--- src/main/kotlin/com/wgtwo/example/Shared.kt
+++ src/main/kotlin/com/wgtwo/example/Shared.kt
@@ -1,9 +1,11 @@
 package com.wgtwo.example
 
+import com.wgtwo.api.common.Environment
 import com.wgtwo.api.util.auth.Clients
 import com.wgtwo.api.util.auth.OperatorToken
+import io.grpc.ManagedChannel
 
 object Shared {
-    val channel = Clients.createChannel(Clients.Environment.PROD)
+    val channel: ManagedChannel = Clients.createChannel(Environment.PROD)
     val credentials = OperatorToken(Secrets.WGTWO_CLIENT_ID, Secrets.WGTWO_CLIENT_SECRET)
 }
