0a8f1037836a15c58afa0fb579db267e396e9289
Didier Parat <didier@wgtwo.com>
2020-02-06 16:21:57 +0100

    Update wgtwo dependency version

--- src/main/kotlin/com/wgtwo/example/voicemail/VoicemailDemo.kt
+++ src/main/kotlin/com/wgtwo/example/voicemail/VoicemailDemo.kt
@@ -1,17 +1,21 @@
 package com.wgtwo.example.voicemail
 
+import com.wgtwo.api.common.v0.PhoneNumberProto
+import com.wgtwo.api.messaging.v0.VoicemailMediaServiceGrpc
+import com.wgtwo.api.messaging.v0.VoicemailProto
+import com.wgtwo.api.messaging.v0.VoicemailProto.GetVoicemailResponse.BytesCase
 import com.wgtwo.example.Shared.channel
 import com.wgtwo.example.Shared.credentials
 import io.grpc.StatusRuntimeException
-import io.omnicate.messaging.protobuf.Voicemail
-import io.omnicate.messaging.protobuf.VoicemailMediaServiceGrpc
 import javax.sound.sampled.AudioSystem
 
 object VoicemailDemo {
     private val blockingStub = VoicemailMediaServiceGrpc.newBlockingStub(channel).withCallCredentials(credentials)
 
-    fun listVoicemails(msisdn: String): MutableList<Voicemail.VoicemailMetadata>? {
-        val voicemailMetadataRequest = Voicemail.GetAllVoicemailMetadataRequest.newBuilder().setMsisdn(msisdn).build()
+    fun listVoicemails(msisdn: String): MutableList<VoicemailProto.VoicemailMetadata>? {
+        val phoneNumberProto = PhoneNumberProto.PhoneNumber.newBuilder().setE164(msisdn)
+        val voicemailMetadataRequest =
+            VoicemailProto.GetAllVoicemailMetadataRequest.newBuilder().setTo(phoneNumberProto).build()
         val metadataResponse = try {
             blockingStub.getAllVoicemailMetadata(voicemailMetadataRequest)
         } catch (e: StatusRuntimeException) {
@@ -19,12 +23,11 @@ object VoicemailDemo {
             return null
         }
 
-        val voicemailList = metadataResponse.voicemailsMetadataList
-        return voicemailList
+        return metadataResponse.metadataList
     }
 
     fun playVoicemail(voicemailId: String) {
-        val voicemailRequest = Voicemail.GetVoicemailRequest.newBuilder().setVoicemailId(voicemailId).build()
+        val voicemailRequest = VoicemailProto.GetVoicemailRequest.newBuilder().setVoicemailId(voicemailId).build()
 
         val voicemail = try {
             blockingStub.getVoicemail(voicemailRequest)
@@ -36,7 +39,12 @@ object VoicemailDemo {
         val tempFile = createTempFile(prefix = "voicemail", suffix = ".wav")
         println(tempFile.absoluteFile)
         val outputStream = tempFile.outputStream()
-        voicemail.voicemailFile.writeTo(outputStream)
+
+        if (voicemail.bytesCase != BytesCase.WAV) {
+            println("Unexpected format for voicemail.")
+            return
+        }
+        voicemail.wav.writeTo(outputStream)
         outputStream.close()
 
         val audioInputStream = AudioSystem.getAudioInputStream(tempFile)
@@ -49,7 +57,7 @@ object VoicemailDemo {
     }
 
     fun markVoicemailAsRead(voicemailId: String): Boolean {
-        val readRequest = Voicemail.MarkVoicemailAsReadRequest.newBuilder().setVoicemailId(voicemailId).build()
+        val readRequest = VoicemailProto.MarkVoicemailAsReadRequest.newBuilder().setVoicemailId(voicemailId).build()
 
         return try {
             blockingStub.markVoicemailAsRead(readRequest)
@@ -61,7 +69,7 @@ object VoicemailDemo {
     }
 
     fun deleteVoicemail(voicemailId: String): Boolean {
-        val deleteRequest = Voicemail.DeleteVoicemailRequest.newBuilder().setVoicemailId(voicemailId).build()
+        val deleteRequest = VoicemailProto.DeleteVoicemailRequest.newBuilder().setVoicemailId(voicemailId).build()
 
         return try {
             blockingStub.deleteVoicemail(deleteRequest)
